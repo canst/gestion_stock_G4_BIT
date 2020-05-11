bc3867e59709a83d8b6aa0503a95ed218d2bca98
canst <soyames@gmail.com>
2020-05-11 11:07:37 +0000

    modification de la page

---
+++ owner.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="keywords" content="">    
+
+  <title>gestionnaire de sctock</title>
+
+  <!-- Bootstrap core CSS -->
+  
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+     <link href="css/w3.css" rel="stylesheet" type="text/css"/>
+
+      <link href="css/main.css" rel="stylesheet">
+  
+    
+</head>
+    
+<body>
+    <section>
+        <div>
+            <form>
+                
+            </form>
+        </div>
+    </section>
+</body>
+</html>

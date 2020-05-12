b86950d697be147e6cbb7023ac8bd13cca52cb64
canst <soyames@gmail.com>
2020-05-12 10:28:59 +0000

    update header in register

--- register.php
+++ register.php
@@ -1,178 +1,3 @@
-<?php
-
-session_start();
-
-	if(isset($_SESSION['id'])){
-		
-		header("Location: index.php");
-        exit();
-	}
-
-//require_once 'scriptsdb/config.php';
-
-//preaparetion des champ d'insertion dans  la base de donnee
-
-
-
-if(isset($_POST['valider'])){
-	
-	$nom = $_POST['name'];
-	 $gender = isset ($_POST['gen']) ? $_POST['gen']:'';
-	$mail = $_POST['mail'];
-    $username = $_POST['pseudo'];
-	$pwd = isset($_POST['pass']) ? $_POST['pass']:'' ;
-	$phone = $_POST['phone'] ;
-	$adress = $_POST['addres'];
-	 $ville = $_POST['city'];
-    
-    
-    if(empty($_POST['pseudo']) || !preg_match("/^[a-zA-Z0-9_]+$/", $_POST['pseudo'])){
-            
-             $erreur = "Tous les champs doivent Ãªtre renseignés. Les espaces et les ponctuations ne sont pas autorisées pour le nom d'utilisateur. Excepter les traits d'union et tirets bas.";
-    }else{
-        
-        
-      if(empty($_POST['mail']) || !filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)){
-            
-             $erreur = "Veuillez renseigner un email valide."; 
-            
-    
-          }else{
-
-                $req =$bdd->prepare("SELECT user_id FROM register WHERE pseudo=?");
-                $req->execute([$username]);
-                $user = $req->fetch();
-
-
-                if($user){
-
-                    $erreur="Ce d'utilisateur existe déja";
-
-                }else{
-
-                $reqs =$bdd->prepare("SELECT user_id FROM register WHERE mail=?");
-                $reqs->execute([$mail]);
-                $usermail = $reqs->fetch();
-
-
-                if($usermail){
-
-                    $erreur="Email exist déja  utilisé";
-                    
-                     //header("Location: login.php?id=".$_SESSION['id']);
-
-                }
-                else{
-                
-                    $req = $bdd->prepare ( "INSERT into register SET  name = ?, gen = ?, mail = ?, pseudo =?, pass =?, phone = ?, addres = ?, city = ?, confirmation_token = ?");
-                   $pwd = password_hash($_POST['pass'], PASSWORD_BCRYPT);
-                    $token = str_random(60);
-                    $req->execute([$nom, $gender,$mail, $username, $pwd, $phone, $adress, $ville, $token ]);
-                    
-                    $user_id = $bdd-> lastInsertId();
-                    
-                   // mail($_POST['mail'], 'Confirmation de votre compte', "afin de valider votre compte merci de cliquer sur ce lien\n\nhttp://local.dev/lab/comptes/confirm.php?id=&token=$token")
-                    
-                        header('Location: login.php');
-                                        
-                    $ok = "Bienvenu dans la famille Agrimarket !!!";
-                    $ok2=" Votre compte a bien été créé. ";
-                    $ok3="Cliquer en bas pour vous connectez";
-                    
-                   
-                   
-
-$email = $mail; // Déclaration de l'adresse de destination.
-if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn|gmail).[a-z]{2,4}$#", $email)) // On filtre les serveurs qui rencontrent des bogues.
-{
-	$passage_ligne = "\r\n";
-}
-else
-{
-	$passage_ligne = "\n";
-}
-//=====Déclaration des messages au format texte et au format HTML.
-$message_txt = "Merci de vous être enregistré sur www.Agrimarket-bf.com ";
-$message_html ="<html><head></head><body><b>Bonjour $nom</b>,<br/><br/>
- Merci de vous être enregistré sur <b>Agrimarket-bf.com</b>. <br/><br/>
-identifiant : $username<br/>
-mot de passe : Celui que vous avez renseigné.<br/><br/>
-L'équipe de agrimarket bf<br/><br/>
-PS: Ceci est un email automatique, merci de ne pas y répondre.
-</body></html>";
-//==========
- 
-//=====Création de la boundary
-$boundary = "-----=".md5(rand());
-//==========
- 
-//=====Définition du sujet.
-$sujet = "Création de compte !";
-//=========
- 
-//=====Création du header de l'e-mail.
-$header = "From: \"Agrimarket bf\"<contact@agrimarket-bf.com>".$passage_ligne;
-$header.= "Reply-to: \"Agrimarket\" <contact@agrimarket-bf.com>".$passage_ligne;
-$header.= "MIME-Version: 1.0".$passage_ligne;
-$header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-//==========
- 
-//=====Création du message.
-$message = $passage_ligne."--".$boundary.$passage_ligne;
-//=====Ajout du message au format texte.
-$message.= "Content-Type: text/plain; charset=\"ISO-8859-1\"".$passage_ligne;
-$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-$message.= $passage_ligne.$message_txt.$passage_ligne;
-//==========
-$message.= $passage_ligne."--".$boundary.$passage_ligne;
-//=====Ajout du message au format HTML
-$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
-$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-$message.= $passage_ligne.$message_html.$passage_ligne;
-//==========
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-$message.= $passage_ligne."--".$boundary."--".$passage_ligne;
-//==========
- 
-//=====Envoi de l'e-mail.
-mail($email,$sujet,$message,$header, $token);
-//==========    
-                     exit();
-	
-                }
-             }
-                }
-		  
-        
-    }
-	
-    
-		
-
-
-}
-
-
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -198,23 +23,37 @@ mail($email,$sujet,$message,$header, $token);
 <body id="page-top">
 <header>
   <!-- Navigation-->
-  <nav class="navbar navbar-expand-lg bg-dark text-uppercase fixed-top" id="mainNav">
+   <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-secondary" id="mainNav">
+     <a href="index.php"><img class="w3-border-teal" src="img/logos/agriProofLogo.png" id="logo" title="logo"></a>
     <div class="container">
-		<a class="navbar-brand js-scroll-trigger" href="#page-top">AgriMarket</a>
-      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">StockApp</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
       </button>
-      <p style="color:yellow;padding-left:80px;">Veuillez vous enregister pour acceder a notre marcher </p>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
+      <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav text-uppercase ml-auto">
-          
+        
           <li class="nav-item">
-            <a class="nav-link js-scroll-t btn btn-info btn-lg-block ames-btn" href="index.php">Retour acceuil</a>
+            <a class="nav-link js-scroll-trigger" href="index.php">acceuil</a>
           </li>
-        </ul>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#">Vente</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">Commande</a>
+          </li>
+          <!--<li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#team">équipe</a>
+          </li>-->
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="login.php">Connexion</a>
+          </li>
+		  </ul>
+         
     </div>
-    </div>
+
+        </div>
   </nav>
 </header>
  

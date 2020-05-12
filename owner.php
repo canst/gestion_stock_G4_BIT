9a56f06d3dcc7685c5a24bed3ed1a0784c2f6ed4
canst <soyames@gmail.com>
2020-05-12 10:38:40 +0000

    update de owner

--- owner.php
+++ owner.php
@@ -30,6 +30,41 @@
 </head>
     
 <body>
+	 <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-secondary" id="mainNav">
+     <a href="index.php"><img class="w3-border-teal" src="img/logos/agriProofLogo.png" id="logo" title="logo"></a>
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">StockApp</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav text-uppercase ml-auto">
+        
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Bilan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#">Ventes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">Commandes</a>
+          </li>
+          <!--<li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#team">équipe</a>
+          </li>-->
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#">Inventaires</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="register.php">Incription</a>
+          </li>
+        </ul>
+         
+    </div>
+
+        </div>
+  </nav>
     <section>
         <div>
             <form>
